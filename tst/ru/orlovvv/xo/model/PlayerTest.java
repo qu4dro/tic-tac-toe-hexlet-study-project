87f83e1e258a5352a4fe80125e78a00850abae40
qu4dro <qu4dro.sl@gmail.com>
2020-01-13 19:52:51 +0800

    Player test created

--- tst/ru/orlovvv/xo/model/PlayerTest.java
+++ tst/ru/orlovvv/xo/model/PlayerTest.java
@@ -8,9 +8,25 @@ class PlayerTest {
 
     @Test
     void getName() {
+        final String testName = "Peter";
+        final String expectedName = testName;
+
+        final Player player = new Player(testName, null);
+
+        final String actualName = player.getName();
+
+        assertEquals(expectedName, actualName);
     }
 
     @Test
     void getFigure() {
+        final Figure testValue = Figure.X;
+        final Figure expectedValue = testValue;
+
+        final Player player = new Player(null, testValue);
+
+        final Figure actualFigure = player.getFigure();
+
+        assertEquals(expectedValue, actualFigure);
     }
 }
