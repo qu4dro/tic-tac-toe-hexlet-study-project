b56a0f59f68978da28befcc0fc0a8dc0e1f83b30
qu4dro <qu4dro.sl@gmail.com>
2020-01-14 15:08:20 +0800

    MoveController logic added

--- src/ru/orlovvv/xo/controller/MoveController.java
+++ src/ru/orlovvv/xo/controller/MoveController.java
@@ -1,9 +1,22 @@
 package ru.orlovvv.xo.controller;
 
+import ru.orlovvv.xo.model.Field;
+import ru.orlovvv.xo.model.Figure;
+import ru.orlovvv.xo.model.exceptions.AlreadyOccupiedException;
+import ru.orlovvv.xo.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
 public class MoveController {
 
-    public void applyFigure() {
+    public void applyFigure(final Field field,
+                            final Point point,
+                            final Figure figure) throws AlreadyOccupiedException,
+                                                        InvalidPointException {
 
-
+        if (field.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+        field.setFigure(point,figure);
     }
 }
