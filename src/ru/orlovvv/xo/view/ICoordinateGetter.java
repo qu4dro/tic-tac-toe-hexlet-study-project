34f4f8f0713115c2ddadb6e5c452e8f07fc3c2ce
qu4dro <qu4dro.sl@gmail.com>
2020-01-15 19:14:24 +0800

    AI interface created

---
+++ src/ru/orlovvv/xo/view/ICoordinateGetter.java
@@ -0,0 +1,10 @@
+package ru.orlovvv.xo.view;
+
+import ru.orlovvv.xo.model.Field;
+
+import java.awt.*;
+
+public interface ICoordinateGetter {
+
+    public Point getMoveCoordinate(final Field field);
+}
