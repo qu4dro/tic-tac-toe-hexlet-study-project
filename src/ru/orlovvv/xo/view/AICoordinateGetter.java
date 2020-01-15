200f2145cab228131036b80c6b015cecc584fdd4
qu4dro <qu4dro.sl@gmail.com>
2020-01-15 19:14:45 +0800

    AI class added

---
+++ src/ru/orlovvv/xo/view/AICoordinateGetter.java
@@ -0,0 +1,13 @@
+package ru.orlovvv.xo.view;
+
+import ru.orlovvv.xo.model.Field;
+
+import java.awt.*;
+
+public class AICoordinateGetter implements ICoordinateGetter {
+
+    @Override
+    public Point getMoveCoordinate(Field field) {
+        return null;
+    }
+}
